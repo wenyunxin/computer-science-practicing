5baddde734a236aa4783615e9ab32512e0433f32
wenyunxin <wenyunxin24@gmail.com>
2023-06-15 23:53:22 +0800

    6.15

--- write_subtitle.py
+++ write_subtitle.py
@@ -4,18 +4,18 @@ def write_subtitle(filename):
     """import the re package."""
     import re
 
-    """creat the re pattern"""
+    """create the re pattern"""
     pattern = r'\d+\n[\d,:]+ --> [\d,:]+'
     pattern1 = r'<(font.+\/font)>'
 
-    """open file and read it, then use re module to re-write it. then format the result"""
+    """open the file and read it, then use re module to re-write it. then format the result"""
     with open(filename, 'r+', encoding="utf-8") as f:
         read_string = f.read()
         re_string = re.sub(pattern, '', read_string)
         re_string_font = re.sub(pattern1, '', re_string)
-        new_string = re_string_font.replace("\n", " ").replace("   ", "\n")
+        new_string = re_string_font.replace("\n", " ").replace("   ", "\n").replace("  ", " ")
 
-    """create new file and filled with the "new_string" that re-writed-formatted above."""
+    """create a new file and filled with the "new_string" that re-write-formatted above."""
     with open('new_' + filename.replace(filename.split('.')[-1], 'md'), 'w', encoding="utf-8") as f:
         f.write(new_string)
 
